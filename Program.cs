cc3621b2481fa2ec591c9a0a3574c2fa4c50c083
R89126fd <stepherissieri@gmail.com>
2024-08-12 17:05:11 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EjercicioPolimorfismo
+{
+    // Clase base
+    class Animal
+    {
+        public virtual void HacerSonido()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+
+        public virtual void Moverse()
+        {
+            Console.WriteLine("El animal se mueve.");
+        }
+    }
+
+    // Clase derivada 1
+    class Perro : Animal
+    {
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El perro ladra: ¡Guau guau!");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine("El perro corre rápidamente.");
+        }
+    }
+
+    // Clase derivada 2
+    class Gato : Animal
+    {
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El gato maúlla: ¡Miau miau!");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine("El gato camina sigilosamente.");
+        }
+    }
+
