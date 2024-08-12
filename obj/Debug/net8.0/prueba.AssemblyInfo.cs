156d3ad437b7b912629e507565f126bdcbd5d7b9
R89126fd <stepherissieri@gmail.com>
2024-08-12 17:06:12 -0600

    1

--- obj/Debug/net8.0/prueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/prueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3621b2481fa2ec591c9a0a3574c2fa4c50c083")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
